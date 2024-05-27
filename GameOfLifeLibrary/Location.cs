dd9cec46c5959df3c87708f5f5cdb1fa02d613c1
Brian Freeman <brian_d_freeman@comcast.net>
2024-05-27 12:22:42 -0400

    Add location object

---
+++ GameOfLifeLibrary/Location.cs
@@ -0,0 +1,11 @@
+﻿namespace GameOfLifeLibrary;
+
+public class Location(int x, int y)
+{
+    private readonly int x = x;
+    private readonly int y = y;
+
+    public int Y => y;
+
+    public int X => x;
+}
---
+++ GameOfLifeLibraryTest/LocationTests.cs
@@ -0,0 +1,16 @@
+﻿using GameOfLifeLibrary;
+
+namespace GameOfLifeLibraryTest;
+
+[TestClass]
+public class LocationTests
+{
+    [TestMethod]
+    public void CanCreateLocation()
+    {
+        var location = new Location(0, 0);
+        Assert.IsNotNull(location);
+        Assert.AreEqual(0, location.X);
+        Assert.AreEqual(0, location.Y);
+    }
+}
