1debc8daaa0ebd118d81c78f42faef9a60eb0b43
Brian Freeman <brian_d_freeman@comcast.net>
2024-05-27 00:29:47 -0400

    new board test

---
+++ GameOfLifeLibrary/Board.cs
@@ -0,0 +1,13 @@
+﻿namespace GameOfLifeLibrary;
+
+public class Board
+{
+    private List<Cell> cells;
+
+    public Board()
+    {
+        cells = new List<Cell>();
+    }
+
+    public List<Cell> Cells { get => cells; set => cells = value; }
+}
---
+++ GameOfLifeLibraryTest/BoardTests.cs
@@ -0,0 +1,14 @@
+﻿using GameOfLifeLibrary;
+
+namespace GameOfLifeLibraryTest;
+
+[TestClass]
+public class BoardTests
+{
+    [TestMethod]
+    public void NewBoardHasNoCells()
+    {
+        var board = new Board();
+        Assert.AreEqual(0, board.Cells.Count);
+    }
+}
