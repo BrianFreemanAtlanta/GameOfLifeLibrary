96a1eea2e951ee2af8935443f8a8ff42e5671c16
Brian Freeman <brian_d_freeman@comcast.net>
2024-05-27 12:24:20 -0400

    Location coordinate

--- GameOfLifeLibraryTest/LocationTests.cs
+++ GameOfLifeLibraryTest/LocationTests.cs
@@ -13,4 +13,12 @@ public class LocationTests
         Assert.AreEqual(0, location.X);
         Assert.AreEqual(0, location.Y);
     }
+    [TestMethod]
+    public void CreateLocationAtCoordinate()
+    {
+        var location = new Location(5, 10);
+        Assert.IsNotNull(location);
+        Assert.AreEqual(5, location.X);
+        Assert.AreEqual(10, location.Y);
+    }
 }
