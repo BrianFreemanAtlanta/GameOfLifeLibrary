66a4b0f63b804d67769e3ffd38c9a4a4e83b8ea9
Brian Freeman <brian_d_freeman@comcast.net>
2024-05-27 00:12:40 -0400

    refactor to namespace;

--- GameOfLifeLibrary/Cell.cs
+++ GameOfLifeLibrary/Cell.cs
@@ -1,35 +1,30 @@
-﻿
+﻿namespace GameOfLifeLibrary;
 
-
-
-namespace GameOfLifeLibrary
+public class Cell
 {
-    public class Cell
+    private List<Cell> _neighbors;
+
+    public bool IsAlive {  get; set; }
+    public Cell()
     {
-        private List<Cell> _neighbors;
+        IsAlive = true;
+        _neighbors = new List<Cell>();
+    }
 
-        public bool IsAlive {  get; set; }
-        public Cell()
-        {
-            IsAlive = true;
-            _neighbors = new List<Cell>();
-        }
+     public bool NextState(int numberOfNeighbors)
+    {
+        if (numberOfNeighbors == 3) return true;
+        if (IsAlive && numberOfNeighbors == 2) return true;
+        return false;
+    }
 
-         public bool NextState(int numberOfNeighbors)
-        {
-            if (numberOfNeighbors == 3) return true;
-            if (IsAlive && numberOfNeighbors == 2) return true;
-            return false;
-        }
+    public int GetNumberOfNeighbors()
+    {
+        return _neighbors.Where(c=> c.IsAlive).Count();
+    }
 
-        public int GetNumberOfNeighbors()
-        {
-            return _neighbors.Where(c=> c.IsAlive).Count();
-        }
-
-        public List<Cell> Neighbors()
-        {
-            return _neighbors;
-        }
+    public List<Cell> Neighbors()
+    {
+        return _neighbors;
     }
 }
--- GameOfLifeLibraryTest/CellTests.cs
+++ GameOfLifeLibraryTest/CellTests.cs
@@ -1,59 +1,58 @@
 using GameOfLifeLibrary;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace GameOfLifeLibraryTest
+namespace GameOfLifeLibraryTest;
+
+[TestClass]
+public class CellTests
 {
-    [TestClass]
-    public class CellTests
+    [DataTestMethod]
+    [DataRow(0, false)]
+    [DataRow(1, false)]
+    [DataRow(2, true)]
+    [DataRow(3, true)]
+    [DataRow(4, false)]
+    [DataRow(5, false)]
+    [DataRow(6, false)]
+    [DataRow(7, false)]
+    [DataRow(8, false)]
+    public void CellNeighbors(int numberOfNeighbors, bool expected)
     {
-        [DataTestMethod]
-        [DataRow(0, false)]
-        [DataRow(1, false)]
-        [DataRow(2, true)]
-        [DataRow(3, true)]
-        [DataRow(4, false)]
-        [DataRow(5, false)]
-        [DataRow(6, false)]
-        [DataRow(7, false)]
-        [DataRow(8, false)]
-        public void CellNeighbors(int numberOfNeighbors, bool expected)
-        {
-            var cell = new Cell();
-            Assert.AreEqual(expected, cell.NextState(numberOfNeighbors), $"Cell with {numberOfNeighbors} neighbors should return {expected}");
-        }
-        [TestMethod]
-        public void DeadCell2NeighborsStaysDead()
-        {
-            var cell = new Cell() { IsAlive = false };
-            Assert.AreEqual(false, cell.NextState(2));
-        }
-        [TestMethod]
-        public void NewCellIsAlive()
-        {
-            var cell = new Cell();
-            Assert.IsTrue(cell.IsAlive);
-        }
-        [TestMethod]
-        public void NewCellHasNeighborCount0()
-        {
-            var cell = new Cell();
-            Assert.AreEqual(0, cell.GetNumberOfNeighbors());
-        }
-        [TestMethod]
-        public void CellWith1NeighborHasCount1()
-        {
-            var cell = new Cell();
-            cell.Neighbors().Add(new Cell());
-            cell.GetNumberOfNeighbors();
-            Assert.AreEqual(1, cell.GetNumberOfNeighbors());
-        }
-        [TestMethod]
-        public void CellOnlyCountsLiveNeighbors()
-        {
-            var cell = new Cell();
-            cell.Neighbors().Add(new Cell() { IsAlive=false});
-            cell.GetNumberOfNeighbors();
-            Assert.AreEqual(0, cell.GetNumberOfNeighbors());
-        }
+        var cell = new Cell();
+        Assert.AreEqual(expected, cell.NextState(numberOfNeighbors), $"Cell with {numberOfNeighbors} neighbors should return {expected}");
+    }
+    [TestMethod]
+    public void DeadCell2NeighborsStaysDead()
+    {
+        var cell = new Cell() { IsAlive = false };
+        Assert.AreEqual(false, cell.NextState(2));
+    }
+    [TestMethod]
+    public void NewCellIsAlive()
+    {
+        var cell = new Cell();
+        Assert.IsTrue(cell.IsAlive);
+    }
+    [TestMethod]
+    public void NewCellHasNeighborCount0()
+    {
+        var cell = new Cell();
+        Assert.AreEqual(0, cell.GetNumberOfNeighbors());
+    }
+    [TestMethod]
+    public void CellWith1NeighborHasCount1()
+    {
+        var cell = new Cell();
+        cell.Neighbors().Add(new Cell());
+        cell.GetNumberOfNeighbors();
+        Assert.AreEqual(1, cell.GetNumberOfNeighbors());
+    }
+    [TestMethod]
+    public void CellOnlyCountsLiveNeighbors()
+    {
+        var cell = new Cell();
+        cell.Neighbors().Add(new Cell() { IsAlive=false});
+        cell.GetNumberOfNeighbors();
+        Assert.AreEqual(0, cell.GetNumberOfNeighbors());
     }
 }
