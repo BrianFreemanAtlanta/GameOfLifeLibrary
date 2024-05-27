0aef0f824c01303210066bfac0de170e04f698fb
Brian Freeman <brian_d_freeman@comcast.net>
2024-05-27 12:32:31 -0400

    make x infinite

--- GameOfLifeLibrary/Location.cs
+++ GameOfLifeLibrary/Location.cs
@@ -1,11 +1,11 @@
 ﻿namespace GameOfLifeLibrary;
 
-public class Location(int x, int y)
+public class Location(List<int> x, int y)
 {
-    private readonly int x = x;
+    private readonly List<int> x = x;
     private readonly int y = y;
 
     public int Y => y;
 
-    public int X => x;
+    public List<int> X => x;
 }
--- GameOfLifeLibraryTest/LocationTests.cs
+++ GameOfLifeLibraryTest/LocationTests.cs
@@ -8,17 +8,28 @@ public class LocationTests
     [TestMethod]
     public void CanCreateLocation()
     {
-        var location = new Location(0, 0);
+        List<int> x = new List<int>() { 0 };
+        var location = new Location(x, 0);
         Assert.IsNotNull(location);
-        Assert.AreEqual(0, location.X);
+        Assert.AreEqual(x, location.X);
         Assert.AreEqual(0, location.Y);
     }
     [TestMethod]
     public void CreateLocationAtCoordinate()
     {
-        var location = new Location(5, 10);
+        List<int> x = new List<int> { 5 };
+        var location = new Location(x, 10);
         Assert.IsNotNull(location);
-        Assert.AreEqual(5, location.X);
+        Assert.AreEqual(x, location.X);
+        Assert.AreEqual(10, location.Y);
+    }
+    [TestMethod]
+    public void CreateLocationAtCoordinateWithXPastIntMax()
+    {
+        List<int> x1 = new List<int> { int.MaxValue, 5 };
+        var location = new Location(x1, 10);
+        Assert.IsNotNull(location);
+        Assert.AreEqual(x1, location.X);
         Assert.AreEqual(10, location.Y);
     }
 }
