4038e4e948446bbff761e1f55b5c7c11f6f72ce1
Brian Freeman <brian_d_freeman@comcast.net>
2024-05-28 15:34:47 -0400

    Add magnitude object and first test

---
+++ GameOfLifeLibrary/Magnitude.cs
@@ -0,0 +1,43 @@
+﻿
+namespace GameOfLifeLibrary;
+
+/// <summary>
+/// This will be the magnitude for the cell location
+/// represented as a list of integers and a isNegative property
+/// For compatibility with javascript and serialize we'll use UInt32
+/// Javascript max safe number is 2^53 = 9,007,199,254,740,991
+/// This list will be akin to a base UInt number so that
+/// UInt.MaxValue + 1 = {1,0}
+/// {UInt.MaxValue,UInt.MaxValue} + 1 = {1,0,0}
+/// Has unique rules for ==, Equals, ++, --, hash
+/// May need division/multiply rules for zoom in/out but will see
+/// </summary>
+public class Magnitude
+{
+    private int number;
+
+    public Magnitude(int number)
+    {
+        this.number = number;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return base.Equals(obj);
+    }
+
+    public override int GetHashCode()
+    {
+        return base.GetHashCode();
+    }
+
+    public int ToInt()
+    {
+        return number;
+    }
+
+    public override string? ToString()
+    {
+        return base.ToString();
+    }
+}
---
+++ GameOfLifeLibraryTest/MagnitudeTests.cs
@@ -0,0 +1,15 @@
+﻿
+using GameOfLifeLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GameOfLifeLibraryTest;
+[TestClass]
+public class MagnitudeTests
+{
+    [TestMethod]
+    public void CreateMagnitudeWithSingleNumber()
+    {
+        var magnitude = new Magnitude(5);
+        Assert.AreEqual(5, magnitude.ToInt());
+    }
+}
