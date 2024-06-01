31e5a0d27e16a1741931987c04d2befe59c60526
Brian Freeman <brian_d_freeman@comcast.net>
2024-05-31 22:50:46 -0400

    refactor remove cells list use dictionary

--- GameOfLifeLibrary/Board.cs
+++ GameOfLifeLibrary/Board.cs
@@ -6,36 +6,32 @@ namespace GameOfLifeLibrary;
 
 public class Board
 {
-    private List<Cell> cells;
-    private Dictionary<Point, Cell> cellPointDictionary;
+    private readonly Dictionary<Point, Cell> cellPointDictionary;
     public Board()
     {
-        cells = new List<Cell>();
         cellPointDictionary = new Dictionary<Point, Cell>();
     }
 
     public List<Cell> GetCells(bool includeDeadCells = false)
     {
-        return cells.Where(c => includeDeadCells || c.IsAlive).ToList();
+        var list = cellPointDictionary.Where(c => includeDeadCells || c.Value.IsAlive). Select(c => c.Value).ToList();
+        return list;
     }
 
     public void Add(Cell cell)
     {
         cell.Board = this;
-        cells.Add(cell);
         cellPointDictionary[cell.Point] = cell;
     }
 
     public Cell GetCell(int x, int y)
     {
-        //return cellPointDictionary[new Point(x, y)];
         var point = new Point(x, y);
         if (cellPointDictionary.TryGetValue(point, out Cell? cell))
         {
             return cell;
         }
         cell = new Cell(x, y, false);
-        //cells.Add(cell);
         //cellPointDictionary[cell.Point] = cell;
         return cell;
     }
