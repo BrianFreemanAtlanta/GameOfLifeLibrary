f178a0ef2e82549d1367f309caf899edfeee5d61
Brian Freeman <brian_d_freeman@comcast.net>
2024-05-31 23:40:07 -0400

    test return all 8 neighbors

--- GameOfLifeLibraryTest/BoardTests.cs
+++ GameOfLifeLibraryTest/BoardTests.cs
@@ -88,4 +88,27 @@ public class BoardTests
         var neighbors1 = board.GetNeighbors(cellx, celly);
         Assert.IsTrue(neighbors1.Contains(cell2));
     }
+    [TestMethod]
+    public void GetNeighborsFullGets8LiveNeighbors()
+    {
+        var neighbors = new List<Cell>()
+        {
+            new Cell(1, 2),
+            new Cell(1, 0),
+            new Cell(2, 0),
+            new Cell(2, 1),
+            new Cell(2, 2),
+            new Cell(0, 0),
+            new Cell(0, 1),
+            new Cell(0, 2),
+        };
+        var board = new Board();
+        board.Add(new Cell(1, 1));
+        foreach (var neighbor in neighbors)
+        {
+            board.Add(neighbor);
+        }
+        var actual = board.GetNeighbors(1, 1);
+        CollectionAssert.AreEquivalent(neighbors, actual);
+    }
 }
